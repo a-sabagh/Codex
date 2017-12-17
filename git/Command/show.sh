3b2d209fa0cd2b523f65968e6dc735749cc04f09
asabagh <a.sabagh72@gmail.com>
2017-12-17 10:43:39 +0330

    To see the content of a particular file from that commit

--- git/Command/show.sh
+++ git/Command/show.sh
@@ -1,4 +1,6 @@
 #show commit changes
-git show SHA1
+git show [SHA1]
 #To list the files that were changed by a particular commit
-git show --name-only SHA1
+git show --name-only [SHA1]
+#To see the content of a particular file from that commit
+git show [SHA1]:[FILE_PATH]
