9412fd68014f925f65f8fbdb283a4d31f878005c
asabagh <a.sabagh72@gmail.com>
2018-01-06 11:42:39 +0330

    chrarcter set and collate when create database

--- MySQL/Creating-Using-Database.sql
+++ MySQL/Creating-Using-Database.sql
@@ -3,6 +3,9 @@ SHOW DATABASES;
 USE [db_name];
 #create database
 CREATE DATABASE [db_name];
+@CHARACTER SET
+@COLLATE
+CREATE DATABASE [dbname] CHARACTER SET [characterSetName] COLLATE [collateName];
 #show tables
 SHOW TABELES;
 #describe field set of table
