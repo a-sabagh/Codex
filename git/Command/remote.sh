e67f8647c1a4b3d443da04117bdd39ee88838e4e
asabagh <a.sabagh72@gmail.com>
2018-06-17 11:03:02 +0430

    remove remote

--- git/Command/remote.sh
+++ git/Command/remote.sh
@@ -1,5 +1,7 @@
 #remote and add file to Git Hosting Services
 git remote add <alias> <URL>
+#remote remote 
+git remote rm <alias>
 #show fetch and push HTTPS link
 git remote -v
 #change remote URLs
