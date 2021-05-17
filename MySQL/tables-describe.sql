9d775f4ad535e1242a94a94a7df4e1bd2e37c3fa
Abolfazl Sabagh <a.sabagh72@gmail.com>
2021-05-17 07:51:02 +0430

    show tables filtered by expression

---
+++ MySQL/tables-describe.sql
@@ -0,0 +1,6 @@
+#show tables
+SHOW TABLES
+#show tables filtered by special expression
+SHOW TABLES LIKE EXPRESSION
+#show all woocommerce tables in wordpress database
+SHOW TABLES LIKE '%wc_%'tables-describe.sql
