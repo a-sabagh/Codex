693ac8c58dc57ffe66638e423930ff7bc73c2474
Abolfazl Sabagh <a.sabagh72@gmail.com>
2025-02-11 10:21:11 +0330

    new naming convention aliases

--- git/git-aliases.sh
+++ git/git-aliases.sh
@@ -1,10 +1,10 @@
 #### git log -1 --format=%ct####
-git config --global alias.lastCommitTime 'log -1 --format=%ct'
+git config --global alias.last-commit-time 'log -1 --format=%ct'
 #### git log -1 --format=%cd ####
-git config --global alias.lastCommitDate 'log -1 --format=%cd'
+git config --global alias.last-commit-date 'log -1 --format=%cd'
 ### git log --date=short --pretty=format:%ad | sort | uniq -c ###
-git config --global alias.commitCountPerDays 'log --date=short --pretty=format:%ad | sort | uniq -c'
+#commits-per-days
 ### $ git rev-list --count --since=yesterday --before=today HEAD ###
-git config --global alias.commitCountToday 'rev-list --count --since=yesterday --before=today HEAD'
+git config --global alias.today-commit-count 'rev-list --count --since=yesterday --before=today HEAD'
 ### git config --global --get-regexp alias ###
-git config --global alias.aliasList "config --global --get-regexp alias"
+git config --global alias.alias-list "config --global --get-regexp alias"
